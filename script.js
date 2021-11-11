e877a90aca8453710fab3ba2d16d446a4eafa1a3
lopste <80974914+lopste@users.noreply.github.com>
2021-11-10 17:12:46 -0800

    Implementing features
    also splitting code into more functions

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
 const queryString = window.location.search;
 const URLparams = new URLSearchParams(queryString);
-function draw() {
-	var sizeRange = document.getElementById("size");
-	var penSize = 10;
-	
-	var mouseDown = 0;
+var mouseDown = 0;
+function load() {
+	const canvas = document.getElementById("drawing-canvas");
+	const widthDisplay = document.getElementById("width");
+	const heightDisplay = document.getElementById("height");
 	document.body.onmousedown = function() { 
 		mouseDown = 1
 	}
 	document.body.onmouseup = function() {
 		mouseDown = 0
 	}
-	
-	var canvas = document.getElementById("drawing-canvas");
-
 	if (URLparams.get("width")) {
 		canvas.width = URLparams.get("width");
-		
+		widthDisplay.value = URLparams.get("width")
 	} 
 	if (URLparams.get("height")) {
 		canvas.height = URLparams.get("height");
+		heightDisplay.value = URLparams.get("height");
 	}
 	
+}
+
+function clickEvent(e) {
+	var canvas = document.getElementById("drawing-canvas");
 	var ctx = canvas.getContext("2d");
+	var sizeRange = document.getElementById("size");
 	var colorDropdown = document.getElementById("color");
 	var eraserOption = document.getElementById("eraser");
-	function clickEvent(e) {
-		penSize = sizeRange.value
-      // e = Mouse click event.
-		var rect = e.target.getBoundingClientRect();
-		var x = e.clientX - rect.left; //x position within the element.
-		var y = e.clientY - rect.top;  //y position within the element.
-		if (mouseDown) {
-			ctx.fillStyle = colorDropdown.value;
-			if (!(eraserOption.checked)) {
-				ctx.fillRect(x - penSize/2 , y - penSize/2, penSize, penSize);
-			} else {
-				ctx.clearRect(x - penSize/2, y-penSize/2, penSize, penSize);
-			};
+	penSize = sizeRange.value
+	// e = Mouse click event.
+	var rect = e.target.getBoundingClientRect();
+	var x = e.clientX - rect.left; //x position within the element.
+	var y = e.clientY - rect.top;  //y position within the element.
+	if (mouseDown) {
+		ctx.fillStyle = colorDropdown.value;
+		if (!(eraserOption.checked)) {
+			ctx.fillRect(x - penSize/2 , y - penSize/2, penSize, penSize);
+		} else {
+			ctx.clearRect(x - penSize/2, y-penSize/2, penSize, penSize);
 		};
-	}
+	};
+}
+
+function draw() {
+	load();
+	
+	var canvas = document.getElementById("drawing-canvas");
+	var ctx = canvas.getContext("2d");
 	canvas.addEventListener('mousemove', clickEvent);
 };
 
 function addURLParam(url, param, value){
-    var hash       = {};
-    var parser     = document.createElement('a');
+    var hash = {};
+    var parser = document.createElement('a');
 
-    parser.href    = url;
+    parser.href = url;
 
     var parameters = parser.search.split(/\?|&/);
 
@@ -55,7 +63,7 @@ function addURLParam(url, param, value){
         if(!parameters[i])
             continue;
 
-        var ary      = parameters[i].split('=');
+        var ary = parameters[i].split('=');
         hash[ary[0]] = ary[1];
     }
 
@@ -136,9 +144,15 @@ function saveImageFile() {
 	var imgUrl = canvas.toDataURL("images/png");
 	
 	var a = document.createElement('a');
-   	a.href = imgUrl;
-   	a.download = "canvas.png";
-   	document.body.appendChild(a);
-   	a.click();
+   a.href = imgUrl;
+   a.download = "canvas.png";
+   document.body.appendChild(a);
+   a.click();
 	a.remove();
+}
+
+function resetDrawing() {
+	const canvas = document.getElementById("drawing-canvas");
+	canvas.width = canvas.width;
+	canvas.height = canvas.height;
 }
